a100fe3e13e46f2be7c666453f4ea78b47ce82ab
Juno Kim <lippoint.surf0622@gmail.com>
2021-03-02 11:46:08 +0900

    이용약관 웹뷰페이지 생성

--- Screens/Common/Terms/index.js
+++ Screens/Common/Terms/index.js
@@ -1,32 +1,40 @@
 import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView, Dimensions } from 'react-native';
-import Header from '../Header';
+import {View, Text, StyleSheet, SafeAreaView, Dimensions} from 'react-native';
+import {WebView} from 'react-native-webview';
+import AutoHeightWebView from 'react-native-autoheight-webview';
+
+import Header from '../DetailHeader';
 
 const index = (props) => {
   const navigation = props.navigation;
   const routeName = props.route.name;
 
+  const [getHeight, setGetHeight] = React.useState(null);
+
   return (
-    <SafeAreaView style={styles.container}>
+    <>
       <Header title={routeName} navigation={navigation} />
-      <View style={styles.content}>
-        <Text style={styles.normalText}>이용약관</Text>
-      </View>
-    </SafeAreaView>
+
+      <AutoHeightWebView
+        style={{
+          width: Dimensions.get('window').width - 20,
+        }}
+        source={{
+          uri: `http://dmonster1506.cafe24.com/bbs/content.php?co_id=provision`,
+        }}
+        scalesPageToFit={Platform.OS === 'Android' ? true : false}
+        viewportContent={'width=device-width, user-scalable=no'}
+      />
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  },
-  content: {
-    flex: 1,
-    height: Dimensions.get('window').height - 300,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#fff',
   },
   normalText: {
     fontFamily: 'SCDream4',
