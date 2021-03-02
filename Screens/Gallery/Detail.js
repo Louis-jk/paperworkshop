738e95a6f910cb07a55bb4a854955922a0bb26be
Juno Kim <lippoint.surf0622@gmail.com>
2021-03-02 11:45:47 +0900

    불필요한 코드 삭제

--- Screens/Gallery/Detail.js
+++ Screens/Gallery/Detail.js
@@ -22,37 +22,6 @@ const Detail = (props) => {
   const navigation = props.navigation;
   const routeName = props.route.name;
 
-  const phoneNumber = '01012345678';
-  const emailAddress = 'paper_workshop@paperworkshop.com';
-
-  const carouselRef = React.useRef(null);
-
-  const entries = [
-    {
-      id: 1,
-      image: require('../../src/images/p012.jpg'),
-    },
-    {
-      id: 2,
-      image: require('../../src/images/p03.jpg'),
-    },
-    {
-      id: 2,
-      image: require('../../src/images/p04.jpg'),
-    },
-  ];
-
-  const renderItem = ({ item, index }) => {
-    return (
-      <Image
-        key={index}
-        source={item.image}
-        resizeMode="cover"
-        style={{ width: Dimensions.get('window').width, height: 400 }}
-      />
-    );
-  };
-
   return (
     <>
       <DetailHeader title={routeName} navigation={navigation} />
@@ -70,25 +39,37 @@ const Detail = (props) => {
               alignItems: 'center',
               marginBottom: 10,
             }}>
-            <Text style={[styles.normalText, { fontSize: 14, color: '#333333' }]}>삼보인쇄</Text>
-            <Text style={[styles.normalText, { color: '#A2A2A2' }]}>2020.11.01</Text>
+            <Text style={[styles.normalText, {fontSize: 14, color: '#333333'}]}>
+              삼보인쇄
+            </Text>
+            <Text style={[styles.normalText, {color: '#A2A2A2'}]}>
+              2020.11.01
+            </Text>
           </View>
           <View>
-            <Text style={[styles.mediumText, { fontSize: 18, lineHeight: 26 }]}>
+            <Text style={[styles.mediumText, {fontSize: 18, lineHeight: 26}]}>
               [패키지] 병원/의약품/건강/케어/헬스 관련패키지 디자인입니다.
             </Text>
           </View>
         </View>
         <View
-          style={{ width: Dimensions.get('window').width, height: 1, backgroundColor: '#D7D7D7' }}
+          style={{
+            width: Dimensions.get('window').width,
+            height: 1,
+            backgroundColor: '#D7D7D7',
+          }}
         />
-        <View style={{ paddingHorizontal: 20, paddingVertical: 20 }}>
+        <View style={{paddingHorizontal: 20, paddingVertical: 20}}>
           <Image
             source={require('../../src/images/p10.jpg')}
             resizeMode="cover"
-            style={{ width: '100%', height: 370, marginBottom: 20 }}
+            style={{width: '100%', height: 370, marginBottom: 20}}
           />
-          <Text style={[styles.normalText, { fontSize: 15, color: '#333333', lineHeight: 26 }]}>
+          <Text
+            style={[
+              styles.normalText,
+              {fontSize: 15, color: '#333333', lineHeight: 26},
+            ]}>
             내용입니다내용입니다내용입니다내용입니다내용입니다내용입니다내용입니다내용입니다내용입니다내용입니다내용입니다내용입니다내용입니다내용입니다내용입니다내용입니다내용입니다내용입니다
           </Text>
         </View>
