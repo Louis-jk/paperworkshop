80fcf3c56d792f9558822ec785e3c3f9ad9d7685
Juno Kim <lippoint.surf0622@gmail.com>
2021-03-26 16:03:15 +0900

    유용한정보 API 인터셉터 생성

--- src/api/Ccenter.js
+++ src/api/Ccenter.js
@@ -2,12 +2,21 @@ import Send from '../utils/Send.js';
 import qs from 'qs';
 
 export default {
-  onReviews(cate1, search) {
+  getReviews(cate1, search) {
     return Send({
       method: 'post',
       data: qs.stringify({
         method: 'proc_story_review',
         cate1,
+        search,
+      }),
+    });
+  },
+  getTips(search) {
+    return Send({
+      method: 'post',
+      data: qs.stringify({
+        method: 'proc_info_list',
         search,
       }),
     });
