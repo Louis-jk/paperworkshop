0647a4d4c8526e51222c472d5a364ee53bbfe104
Juno Kim <lippoint.surf0622@gmail.com>
2021-04-01 18:38:22 +0900

    통합검색 API

--- src/api/Search.js
+++ src/api/Search.js
@@ -2,6 +2,7 @@ import Send from '../utils/Send.js';
 import qs from 'qs';
 
 export default {
+  // 검색어 히스토리 가져오기 및 추가, 삭제
   getSearchHistory(mb_id, id, keyword, insert, del) {
     return Send({
       method: 'post',
@@ -12,6 +13,16 @@ export default {
         keyword,
         insert,
         delete: del,
+      }),
+    });
+  },
+  // 검색 결과 가져오기
+  getSearchResult(keyword) {
+    return Send({
+      method: 'post',
+      data: qs.stringify({
+        method: 'proc_search',
+        keyword,
       }),
     });
   },
