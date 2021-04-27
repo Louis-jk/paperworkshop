3703c7b2c37e7fb87b46ccbc1a16bfb06975d42f
Juno Kim <lippoint.surf0622@gmail.com>
2021-04-27 14:23:22 +0900

    지류 정보 API 인터셉터 추가

--- src/api/BoxType.js
+++ src/api/BoxType.js
@@ -12,6 +12,7 @@ export default {
       }),
     });
   },
+  // 
   getBoxTypeId(cate1, ca_id, type_id) {
     return Send({
       method: 'post',
@@ -23,7 +24,57 @@ export default {
       }),
     });
   },
-  getColorInfo(pd_id,paper_name2,paper_weight) {
+  // 지류 초기값 가져오기
+  getPaperInitialInfo(cate1, ca_id, type_id){
+    return Send({
+      method: 'post', 
+      data: qs.stringify({
+        method: 'proc_feeder_list',
+        cate1,
+        ca_id,
+        type_id
+      })
+    })
+  },
+  // 지류 해당 상세 정보 가져오기 - 경우에 따라 표지용
+  getPaper1DepthInfo(cate1, ca_id, pf_id) {
+    return Send({
+      method: 'post', 
+      data: qs.stringify({
+        method: 'proc_paper_list',
+        cate1,
+        ca_id,
+        pf_id
+      })
+    })
+  },
+  // 지종 가져오기(1차)
+  getPaperDetailInfo(ca_id, pd_id) {
+    return Send({
+      method: 'post', 
+      data: qs.stringify({
+        method: 'proc_paper_detail_list',
+        ca_id,
+        pd_id
+      })
+    })
+  },
+  // 지종 가져오기(지종세부 없는 경우)
+  getPaperNoDetailInfo(cate1, ca_id, pf_id, pd_id, paper_name) {
+    return Send({
+      method: 'post', 
+      data: qs.stringify({
+        method: 'proc_paper_list_nodetail',
+        cate1,
+        ca_id,
+        pf_id,
+        pd_id,
+        paper_name
+      })
+    })
+  },
+  // 색상 정보 가져오기
+  getColorInfo(pd_id, paper_name2, paper_weight) {
     return Send({
       method: 'post', 
       data: qs.stringify({
@@ -31,6 +82,17 @@ export default {
         pd_id,
         paper_name2,
         paper_weight
+      })
+    })
+  },
+  // 인쇄도수/인쇄교정/인쇄감리 가져오기
+  getCheckPrint(cate1, ca_id) {
+    return Send({
+      method: 'post', 
+      data: qs.stringify({
+        method: 'proc_check_print',
+        cate1,
+        ca_id
       })
     })
   }
